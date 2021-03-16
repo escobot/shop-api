dc6e6dbc55b1043f441f0cc2f94e2649bac6000e
pbgnz <pabgonza1995@gmail.com>
2021-03-16 17:03:04 -0400

    add mongo config

---
+++ src/main/java/com/example/shop/config/MongoConfig.java
@@ -0,0 +1,20 @@
+package com.example.shop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.*;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory mongoDatabaseFactory,
+                                                       MongoMappingContext mongoMappingContext) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+        MappingMongoConverter mappingMongoConverter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return  mappingMongoConverter;
+    }
+}
